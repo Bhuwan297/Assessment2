a13d04d2c6d94b395a4b213e41cd95c9e741151a
Bhuwan297 <66291102+Bhuwan297@users.noreply.github.com>
2024-08-05 18:26:36 +1000

    header information
    header information and file opening process.

--- Assessment2/StudentDataFormatter.java
+++ Assessment2/StudentDataFormatter.java
@@ -12,9 +12,22 @@ public class StudentDataFormatter {
         
         // and here we are Asking the user for the name of the file to process
         System.out.print("Please enter the filename (e.g., file.csv): ");
-        String fileName = inputScanner.nextLine(); // Reading the filename entered by the user
+        String fileName = inputScanner.nextLine(); // here we are Reading the filename entered by the user
         
+        // for the context, we are providing header information about the application
+        System.out.println("\nThis application shows the detailed information and in clean format of student assessments of Fundamentals of Programming");
+        System.out.println("UNIT: Fundamentals of Programming");
         
+        // this line is to Adjust all column widths to 30% cause the files are not in same length and same size
+        System.out.println(String.format("%-30s %-30s %-30s %-30s %-30s %-30s", "Student Last Name", "First Name", "Student ID", "A1", "A2", "A3"));
+        System.out.println("----------------------------------------------------------------------------------------------------");
+
+        // Now we Attempt to open the file and process it
+        try {
+            // first we Create a file instance with the given file name
+            File file = new File(fileName);
+            // then we Open a scanner to read from the file
+            Scanner fileScanner = new Scanner(file);
         
 
 
