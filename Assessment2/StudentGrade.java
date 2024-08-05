f2283e1233dab4c4e265cadc9facef65bcbed52b
Bhuwan297 <66291102+Bhuwan297@users.noreply.github.com>
2024-08-05 17:31:26 +1000

    initiation
    initiation

---
+++ Assessment2/StudentGrade.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class StudentGrade{
+
+    private static List <Student> students = new ArrayList <Student>();
+    
+    public static void main 
+    
+
+
+
+
+
+
+}
