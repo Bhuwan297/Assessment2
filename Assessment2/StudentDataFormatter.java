c5f7ecf0ccfd33b73c728bf72f10620200835144
Bhuwan297 <66291102+Bhuwan297@users.noreply.github.com>
2024-08-05 17:50:03 +1000

    created a scanner for reading input
    scanner and file reading method.

--- Assessment2/StudentDataFormatter.java
+++ Assessment2/StudentDataFormatter.java
@@ -2,9 +2,20 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-class StudentDataFormatter {
+public class StudentDataFormatter {
+    
+    
+    
+    public static void main(String[] args) {
+        // here we are Creating a scanner object to read input from the console
+        Scanner inputScanner = new Scanner(System.in);
+        
+        // and here we are Asking the user for the name of the file to process
+        System.out.print("Please enter the filename (e.g., file.csv): ");
+        String fileName = inputScanner.nextLine(); // Reading the filename entered by the user
 
 
+    }
 
 
 }
